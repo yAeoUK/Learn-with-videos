d86ef928eb81be39d479f5d5ffc0f69d529a3b5f
Ahmed Rajab <ahmad.rajab@windowslive.com>
2020-07-28 17:27:28 +0300

    configure open database

---
+++ lib/database.dart
@@ -0,0 +1,19 @@
+const WATCHED_VIDEOS='watchedVideos';
+const VIDEO_ID='videoId';
+const REACHED_SECOND='reachedSecond';
+const NOTE='note';
+
+const TABLE_FAVORITE_VIDEOS='favoriteVideos';
+const VIDEO_TITLE='videoTitle';
+///VIDEO_ID
+const VIDEO_DESCRIPTION='videoDescription';
+const VIDEO_THUMBURL='videoThumbURL';
+
+const CHANNELS='channels';
+const ID='id';
+const NAME='name';
+const LINK='link';
+const PICTURE_LINK='pictureLink';
+const VIEWED='viewed';
+const GET_NOTIFICATIONS='getNotifications';
+const LAST_VIDEO_TITLE='lastVideoTitle';
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:catcher/catcher.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:videos/c.dart';
+import 'package:videos/database.dart';
 
 import 'home.dart';
 
 void main() {
-  CatcherOptions debugOptions =
-      CatcherOptions(DialogReportMode(), [ConsoleHandler()]);
-      
+  CatcherOptions debugOptions =CatcherOptions(DialogReportMode(), [ConsoleHandler()]);
   CatcherOptions releaseOptions = CatcherOptions(DialogReportMode(), [
     EmailManualHandler(["ahmad.rajab@windowslive.com"])
   ]);
@@ -19,11 +18,27 @@ class MyApp extends StatelessWidget {
 
   static var database;
 
+  void configureDatabase()async{
+    database = openDatabase(DATABASE_NAME, version: DATABASE_VERSION,
+    onCreate: (Database db, int version) async {
+      await db.execute('create table $WATCHED_VIDEOS ($VIDEO_ID text,$REACHED_SECOND integer default 0,$NOTE text)');
+      await db.execute('create table $TABLE_FAVORITE_VIDEOS ($VIDEO_TITLE text,$VIDEO_ID text,$VIDEO_DESCRIPTION text,$VIDEO_THUMBURL text)');
+      await db.execute('create table $CHANNELS ($ID integer ,$NAME text,$LINK text, $PICTURE_LINK text,$VIEWED integer default 0,$GET_NOTIFICATIONS integer default 1,$LAST_VIDEO_TITLE text)');
+     },
+     onUpgrade: (db, oldVersion, newVersion)async {
+       var tableColumns= await db.query('PRAGMA table_info($WATCHED_VIDEOS)');
+       bool noteColumnExists=false;
+       for(int c=0;c<tableColumns.length;c++){
+         if(tableColumns[c]['name'].toString()==NOTE)noteColumnExists=true;
+       }
+       if(!noteColumnExists) await db.execute('alter table $WATCHED_VIDEOS add $NOTE text');
+     },);
+
   }
 
   @override
   Widget build(BuildContext context) {
-    openDatabase();
+    configureDatabase();
     return MaterialApp(
       navigatorKey: Catcher.navigatorKey,
       title: APP_NAME,
